fde607c718a74f3261e3770f7151a99bd18020b9
mDoce <matias.serro@gmail.com>
2024-08-20 12:09:03 -0300

    Agregada la clase Locomotora con sus metodos implementados

--- EjercicioTrenesYDepositos/src/code/Locomotora.java
+++ EjercicioTrenesYDepositos/src/code/Locomotora.java
@@ -1,2 +1,54 @@
-package code;public class Locomotora {
+package code;
+
+public class Locomotora {
+    private double peso;
+    private double pesoDeArrastreMaximo;
+    private double velocidadMaxima;
+    private Estado estado;
+
+    public Locomotora(double peso, double pesoDeArrastreMaximo, double velocidadMaxima, Estado estado) {
+        this.peso = peso;
+        this.pesoDeArrastreMaximo = pesoDeArrastreMaximo;
+        this.velocidadMaxima = velocidadMaxima;
+        this.estado = estado;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getPesoDeArrastreMaximo() {
+        return pesoDeArrastreMaximo;
+    }
+
+    public void setPesoDeArrastreMaximo(double pesoDeArrastreMaximo) {
+        this.pesoDeArrastreMaximo = pesoDeArrastreMaximo;
+    }
+
+    public double getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+    public void setVelocidadMaxima(double velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public double getArrastreUtil() {
+        return this.pesoDeArrastreMaximo - this.peso;
+    }
+    public boolean esEficiente() {
+        return this.getArrastreUtil() > 5 * this.peso;
+    }
 }
