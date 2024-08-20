4ac327b3bfae0439a5d5dbdfc64a9cf72dde256c
mDoce <matias.serro@gmail.com>
2024-08-20 15:33:46 -0300

    Agregados los tests unitarios para la clase VagonDeCarga

---
+++ EjercicioTrenesYDepositos/src/test/VagonDeCargaTest.java
@@ -0,0 +1,53 @@
+package test;
+
+import code.VagonDeCarga;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VagonDeCargaTest {
+
+    private VagonDeCarga vagon;
+
+    @BeforeEach
+    void setUp() {
+        vagon = new VagonDeCarga(1000);
+    }
+
+    @Test
+    @DisplayName("Verifica el funcionamiento del getCargaMaxima")
+    void getCargaMaxima() {
+        assertEquals(1000, vagon.getCargaMaxima());
+    }
+
+    @Test
+    @DisplayName("Verifica el funcionamiento del setCargaMaxima")
+    void setCargaMaxima() {
+        vagon.setCargaMaxima(1500);
+        assertEquals(1500, vagon.getCargaMaxima());
+    }
+
+    @Test
+    @DisplayName("Verifica el funcionamiento del getPesoMaximo")
+    void getPesoMaximo() {
+        assertEquals(1160, vagon.getPesoMaximo());
+    }
+
+    @Test
+    @DisplayName("Verifica que retorne 0, ya que no puede llevar pasajeros")
+    void getCantidadMaximaPasajeros() {
+        assertEquals(0, vagon.getCantidadMaximaPasajeros());
+    }
+
+    @Test
+    @DisplayName("Verifica que sea liviano si pesa menos de 2500, y que no lo sea si pesa más de 2500")
+    void esLiviano() {
+        VagonDeCarga vagonLiviano = new VagonDeCarga(1700);
+        assertTrue(vagonLiviano.esLiviano());
+
+        VagonDeCarga vagonPesado = new VagonDeCarga(2700);
+        assertFalse(vagonPesado.esLiviano());
+    }
+}
