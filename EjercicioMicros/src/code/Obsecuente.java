af2b9465bc2f499a1869d305b73d5799ba773248
mDoce <matias.serro@gmail.com>
2024-08-26 12:56:14 -0300

    Añadido el constructor en la clase Obsecuente

--- EjercicioMicros/src/code/Obsecuente.java
+++ EjercicioMicros/src/code/Obsecuente.java
@@ -3,6 +3,11 @@ package code;
 public class Obsecuente implements Perfil {
 
     private Pasajero jefe;
+
+    public Obsecuente(Pasajero jefe) {
+        this.jefe = jefe;
+    }
+
     @Override
     public boolean quiereSubir(Micro m) {
         return this.jefe.quiereSubir(m);
